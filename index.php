a0e1b3477709257ef986369dd5a3b5cb959a149b
Daniel <55406355+kotaidaniel@users.noreply.github.com>
2019-10-11 07:36:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+    $username = "root";
+    $password = "";
+    $dbname = "webshop";
+    $servername = "localhost";
+
+    $conn = mysqli_connect($servername, $password, $dbname, $servername);
+
+    if($conn ->connect_error){
+        die("fail".$conn->connect_error);
+    }
+
+    $sql = "SELLECT * from products";
+    $result = $conn->query($sql);
+    var_dump($result);
 
 ?>
